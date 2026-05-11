c2abe4768aac8d251db720144371bf380aaa5a48
Shafkat Ali <uyghurkid@gmail.com>
2026-05-11 13:29:35 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1>Uyghur English Learning App</h1>
+
+      <h2>Alphabet</h2>
+      <p>A - Apple - ئالما</p>
+      <p>B - Book - كىتاب</p>
+      <p>C - Cat - مۈشۈك</p>
+
+      <h2>Vocabulary</h2>
+      <p>Hello = ياخشىمۇسىز</p>
+      <p>Water = سۇ</p>
+      <p>Food = تاماق</p>
+
+      <h2>Quiz</h2>
+      <p>What does “Water” mean?</p>
+      <button>سۇ</button>
+      <button>كىتاب</button>
+    </div>
+  );
+}
+
+export default App;
